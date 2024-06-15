e2b40e440ec49a5b56bce2878d3e556d032e8452
fajar1206436 <fajardwimarsono@gmail.com>
2024-06-15 22:19:33 +0700

    Update 15 Jun

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -12,30 +12,28 @@ class CompanyController extends Controller
     public function all(Request $request)
     {
         $id = $request->input('id');
-        $name =$request->input('name');
+        $name = $request->input('name');
         $limit = $request->input('limit', 10);
 
-        
-        if ($id) {     
-            $company = Company::with(['users'])->find($id);
+        // Jika parameter id ada, cari perusahaan berdasarkan id
+        if ($id) {
+            $company = Company::with('users')->find($id);
 
-            if ($company)
-            {
-                return ResponFormatter::succes($company, 'Company found');
+            if ($company) {
+                return ResponseFormatter::success($company, 'Company found');
             }
 
-            return ResponFormatter::error('Company not found', 404);
+            return ResponseFormatter::error('Company not found', 404);
         }
 
-        $companies = Company::with(['users']);
-
+        // Jika parameter name ada, filter perusahaan berdasarkan nama
+        $query = Company::with('users');
         if ($name) {
-            $companies->where('name', 'like', '%' . $name . '%');
+            $query->where('name', 'like', '%' . $name . '%');
         }
 
-        return ResponseFormatter::success(
-            $companies->paginate($limit),
-            'Companies found'
-        );
+        // Paginasi dan kembalikan data perusahaan
+        $companies = $query->paginate($limit);
+        return ResponseFormatter::success($companies, 'Companies found');
     }
 }
